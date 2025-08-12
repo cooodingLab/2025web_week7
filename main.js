5a4cb57d281968a027830637fa92f7561e0636a3
monster <joycechen.event@gmail.com>
2025-08-12 15:16:41 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+import './assets/scss/all.scss';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+console.log('Hello world');
+
+// AOS 動畫全域設定
+AOS.init({
+  offset: 120,
+  duration: 400,
+  once: false,
+  delay: 0,
+});
